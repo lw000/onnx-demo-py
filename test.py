b0bb1ad6af7658c3ad7bd18ae00cd1ab9a3bd3e1
LW23133\lwsta <373102227@qq.com>
2026-04-24 18:02:02 +0800

    删除

--- test.py
+++
@@ -1,8 +0,0 @@
-import onnx
-
-model = onnx.load("models/train_igbt_model.onnx")
-params = 0
-for initializer in model.graph.initializer:
-    # 计算每个张量的元素个数并累加
-    params += onnx.numpy_helper.to_array(initializer).size
-print(f"参数量: {params:,}")
